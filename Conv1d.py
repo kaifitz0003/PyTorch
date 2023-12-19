814fc71768759b10a64f66610a291cd4a6d24c74
kaifitz0003 <112199083+kaifitz0003@users.noreply.github.com>
2023-12-18 19:44:52 -0500

    Update Conv1d.py

--- Conv1d.py
+++ Conv1d.py
@@ -5,6 +5,7 @@ A kernel is a short signal that either you pick or the ML learns it.
 Then you convolve the kernel with the input to get the output.
 Convolution is a sliding dot product, where you slide the kernel over the input.
 Then, you multiply each point of the kernel with the corresponding point and you add the result. This is the dot product.
+
 '''
 
 ## NUMPY
@@ -17,7 +18,7 @@ y_length = len(X)-len(w)+1 # Finds how many times you need to slide the kernel o
 y1 = np.zeros(y_length) # Place Holder for Output Signal
 
 
-for i in range (y_length): # len(X)-len(w)+1 represents how many times the the kernel slides over the input signal
+for i in range (y_length): # len(X)-len(w)+1 represents how many times the kernel slides over the input signal
   y1[i] = np.dot(X[i:i+len(w)], w) # Manually slides the kernel by taking the parts of the data with len(w)
 
 print(y1)
@@ -25,7 +26,7 @@ print(y1)
 ## PYTORCH
 
 import torch
-x_torch = torch.tensor([[1.,3,0,2,10,1,2,0]])
+x_torch = torch.tensor([[1.,3,0,2,10,1,2,0]]) # This is a single channel
 
 algo = torch.nn.Conv1d(in_channels=1, out_channels = 1, kernel_size = 3)
 algo.weight.data = torch.tensor([[[1.0,2,3]]])
