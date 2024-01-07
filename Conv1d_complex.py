49c2492e5885c596bccf4860415cf3acbb8db783
kaifitz0003 <112199083+kaifitz0003@users.noreply.github.com>
2024-01-06 20:50:28 -0500

    Added comments about weights and bias

--- Conv1d_complex.py
+++ Conv1d_complex.py
@@ -21,14 +21,14 @@ N, Cin, L = x.shape
 Cout = 6
 K = 2
 # Cout changes the number of 2d arrays, Cin changes the number of rows,  and kernel changes the number of columns
-algo = torch.nn.Conv1d(in_channels=Cin, out_channels = Cout, kernel_size = K)
-algo.weight.data = torch.tensor([[[1,2],[3,4],[5,6]],
+algo = torch.nn.Conv1d(in_channels=Cin, out_channels = Cout, kernel_size = K) # Picks the weights and bias at random
+algo.weight.data = torch.tensor([[[1,2],[3,4],[5,6]], # Optional. Set your own weights
                                  [[7,8],[9,10],[11,12]],
                                  [[13,14],[15,16],[17,18]],
                                  [[19,20],[21,22],[23,24]],
                                  [[25,26],[27,28],[29,30]],
                                  [[31,32],[33,34],[35,36]]], dtype = torch.float32)
-algo.bias.data = torch.tensor([1,2,3,4,5,6], dtype = torch.float32) # Bias is how much you add the the convolution.
+algo.bias.data = torch.tensor([1,2,3,4,5,6], dtype = torch.float32) # Bias is how much you add the the convolution Optional. Set your own bias
 
 # OUTPUT
 y = algo(x)
