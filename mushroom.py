16d9e47d772b5c9d7ce8f78051acb2b6d11c83c0
kaifitz0003 <112199083+kaifitz0003@users.noreply.github.com>
2024-09-23 19:26:47 -0400

    Update mushroom.py

--- mushroom.py
+++ mushroom.py
@@ -1,3 +1,20 @@
+"""
+This is a binary classification problem that uses the mushroom dataset to predict whether a mushroom is poisonous or not.
+
+The data is from kaggle https://www.kaggle.com/datasets/prishasawhney/mushroom-dataset. It has 54035 rows and 9 columns.
+
+We used a neural network with 1 hidden layer to create the model.
+The input layer has 8 neurons, the hidden layer has 2 neurons and the output layer has 1 neuron.
+The model is trained for 250 epochs.
+BCELoss made the algorithm learn better than MSELoss.
+
+On a Nvidia T4 GPU on Google Colab it takes around 1 minute per epoch. 250 epochs got an accuracy of around 75%
+
+TODO:
+Improve our results by doing hyperparameter tuning/optimization.
+This means we are checking how many layers, how many neurons per layer, the learning rate and the number of epochs produce  the best results.
+"""
+
 import torch
 import torch.nn as nn
 import numpy as np
@@ -12,12 +29,12 @@ X = df.drop('class', axis = 1).values
 y = df['class'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 X_train.shape, X_test.shape, y_train.shape, y_test.shape
-model = nn.Sequential(nn.Linear(8,1),nn.Sigmoid(),nn.Linear(1,1),nn.Sigmoid())
+model = nn.Sequential(nn.Linear(8,2),nn.Sigmoid(),nn.Linear(2,1),nn.Sigmoid())
 loss_fn = nn.BCELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.1)
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
 X_train = torch.from_numpy(X_train.astype(np.float32))
 y_train = torch.from_numpy(y_train.astype(np.float32))
-for epoch in range(20):
+for epoch in range(250):
   for i in range(len(X_train)):
     out = model(X_train[i])
     loss = loss_fn(out[0], y_train[i])
