ade09bda62801b02cd7ae643a87f9dab57fb6afa
kaifitz0003 <112199083+kaifitz0003@users.noreply.github.com>
2024-11-09 20:38:44 -0500

    Update DataLoader_and_TensorDataset.py

--- DataLoader_and_TensorDataset.py
+++ DataLoader_and_TensorDataset.py
@@ -1,10 +1,14 @@
+"""
+https://pytorch.org/docs/stable/data.html#torch.utils.data.TensorDataset
+"""
 import torch
 from torch import nn
 from torch.utils.data import TensorDataset,DataLoader
 # Supervised learning
 X = torch.tensor([[1,2,3],[4,5,6],[7,8,9],[10,11,12],[13,14,15]])
 y = torch.tensor([16,17,18,19,20])
-dataset = TensorDataset(X,y)
+dataset = TensorDataset(X,y) # The benefit of having a TensorDataset is it combines X and y. 
+
 
 out = DataLoader(dataset, shuffle=True,batch_size = 1)
 for i,j in out:
